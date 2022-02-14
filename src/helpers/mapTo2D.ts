2ac86c15f77a4c29f0d008e5f6298cb304c2efe4
Lukas Wagner <lukas@lwgnr.dev>
2022-02-14 14:32:26 +0100

    more debug logs, debug grid

--- src/helpers/mapTo2D.ts
+++ src/helpers/mapTo2D.ts
@@ -2,6 +2,12 @@ import { mat4, vec2, vec4 } from 'gl-matrix';
 import { Source } from '../types/source';
 
 export function mapTo2D(points: Source, matrix: mat4): vec2[] {
+    if(window.verbose) {
+        console.log('mapping to 2D');
+        console.log('matrix:', matrix);
+        console.log('input:', points);
+    }
+
     const result: vec2[] = [];
     for (let i = 0; i < points.length; i++) {
         const p3 = points.at(i);
@@ -10,5 +16,10 @@ export function mapTo2D(points: Source, matrix: mat4): vec2[] {
         const p2 = vec2.fromValues(p4[0] / p4[3], p4[1] / p4[3]);
         result.push(p2)
     }
+
+    if(window.verbose) {
+        console.log('output:', result);
+    }
+
     return result;
 }
--- src/helpers/mapToPixels.ts
+++ src/helpers/mapToPixels.ts
@@ -1,8 +1,22 @@
 import { vec2 } from 'gl-matrix';
 
 export function mapToPixels(points: vec2[], resolution: vec2): vec2[] {
-    if(window.verbose)
+    if(window.verbose) {
         console.log('mapping to pixels');
+        console.log('resolution:', resolution);
+        console.log('input:', points);
+    }
 
-    return points.map((p) => vec2.mul(vec2.create(), p, resolution));
+    const result = points.map((p) => {
+        const v = vec2.create();
+        vec2.scaleAndAdd(v, [0.5, 0.5], p, 0.5);
+        vec2.mul(v, v, resolution);
+        return v;
+    });
+
+    if(window.verbose) {
+        console.log('output:', result);
+    }
+
+    return result;
 }
